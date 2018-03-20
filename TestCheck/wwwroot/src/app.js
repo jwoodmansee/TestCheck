6d1521e45ee39fc2d39184ec6bd1efbb938a9c9c
Justin Woodmansee <jwoodmansee@wilsonelectronics.com>
2018-03-20 15:57:24 -0600

    initial commit

---
+++ TestCheck/wwwroot/src/app.js
@@ -0,0 +1,12 @@
+﻿import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Router from './components/Router';
+
+ReactDOM.render(
+	<Router />,
+	document.getElementById('app')
+);
+
+module.hot.accept();
